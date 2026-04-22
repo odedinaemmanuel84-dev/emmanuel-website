f536919e21f4d57fdda564005bf73b82bfb286ad
odedinaemmanuel84-dev <odedinaemmanuel84@gmail.com>
2026-04-22 14:21:26 +0100

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,8 @@
-
+// Simple scroll effect
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
