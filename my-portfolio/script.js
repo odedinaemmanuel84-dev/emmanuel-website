398e7c038e02b058bf7ab592b26c99a4b13eeac5
odedinaemmanuel84-dev <odedinaemmanuel84@gmail.com>
2026-05-12 13:36:04 +0100

    Refactor header scroll effect and add particles.js
    Removed scroll event listener for header background change and added particles.js initialization.

--- my-portfolio/script.js
+++ my-portfolio/script.js
@@ -1,9 +1,3 @@
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
-
-  if(window.scrollY > 50) {
-    header.style.background = '#050816';
-  } else {
-    header.style.background = 'rgba(0,0,0,0.5)';
-  }
+particlesJS("particles-js",{
+particles:{number:{value:80},color:{value:"#00ffff"},size:{value:3},move:{speed:2}}
 });
